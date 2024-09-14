ba2aa7a725e8a54ba70a4d0088c6f544fa5fa567
harukari007 <harukari007@gmail.com>
2024-09-14 18:23:33 +0900

    表示部分変更

--- wagmi-project/src/components/sidebar.tsx
+++ wagmi-project/src/components/sidebar.tsx
@@ -6,12 +6,12 @@ export default function Sidebar() {
   return (
     <div className="flex flex-col justify-around items-center min-h-screen">
       <div className="w-36">
-        <h1 className="text-xl font-semibold">受講生</h1>
+        <h1 className="text-xl font-semibold">刀DAO</h1>
         <nav className="grid gap-4 text-sm text-muted-foreground mt-6">
-        <Link href={paths.proposals()}>Proposals</Link>
-        <Link href={paths.memberJoin()}>Member Join</Link>
-        <Link href={paths.members()}>Members</Link>
-          <Link href={paths.vote()}>Vote</Link>
+        <Link href={paths.proposals()}>提案</Link>
+        <Link href={paths.memberJoin()}>参加会員</Link>
+        <Link href={paths.members()}>刀DAOメンバー</Link>
+          <Link href={paths.vote()}>投票</Link>
         </nav>
       </div>
       <div className="w-36">
